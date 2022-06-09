ec9f14611994caadbeb742553a5c1cfb11c1e74b
Gopika-gs <gsgopika00@gmail.com>
2022-06-09 19:09:15 +0530

    txt file added

--- L3_Q1.py
+++
@@ -1,8 +0,0 @@
-from importlib.resources import contents
-
-
-with open('readme.txt') as f:
-    number_of_line = 3
-    for i in range(number_of_line):
-        content = f.readline()
-        print(content)
